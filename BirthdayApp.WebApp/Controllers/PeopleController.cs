4b5b5d334ebbe3bc9f9c9b575b1d716a7d6edd43
Bruno Feijó <bruno.feijo@live.com>
2020-06-08 15:45:22 -0300

    WebApp add

---
+++ BirthdayApp.WebApp/Controllers/PeopleController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BirthdayApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BirthdayApp.Business;
+
+namespace BirthdayApp.WebApp.Models
+{
+    public class PeopleController : Controller
+    {
+
+        public PeopleController()
+        {
+            Db = new DatabaseFile();
+        }
+
+        private readonly Database Db;
+
+        // GET: People
+        public ActionResult Index()
+        {
+            var people = Db.GetAllPeople();
+            return View(people);
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Person person)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Db.SalvePerson(person);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: People/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: People/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
