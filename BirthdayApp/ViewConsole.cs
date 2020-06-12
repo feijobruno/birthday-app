ededc6fb94d4c07eeb59c97d4833928c3171a909
Bruno Feijó <bruno.feijo@live.com>
2020-06-11 23:12:40 -0300

    CRUD in file's Datase Create Abstract Class for Repository's People Create Class to Featured Text

--- BirthdayApp/ViewConsole.cs
+++ BirthdayApp/ViewConsole.cs
@@ -14,12 +14,8 @@ namespace BirthdayApp
         public static void MainMenu()
         {
             Console.Clear();
-            ContrastColor("Yellow");
-            Console.WriteLine("Gerenciamento de Aniversários de Amigos");
-            Console.ResetColor();
-            ContrastColor("Blue");
-            Console.WriteLine("\nAniversariante(s) do dia:");
-            Console.ResetColor();
+            FeaturedText("Gerenciamento de Aniversários de Amigos", "Yellow");
+            FeaturedText("\nAniversariante(s) do dia:", "Blue");
             ShowBirthdayToday();
             Console.WriteLine("\nSelecione uma das opções abaixo: ");
             Console.WriteLine("1 - Pesquisar pessoas ");
@@ -77,9 +73,7 @@ namespace BirthdayApp
                 Console.Clear();
                 var person = new Person(Db.GetSequenceId(), firstName, lastName, birthday);
                 Db.SalvePerson(person);
-                ContrastColor("Green");
-                Console.WriteLine("Dados adicionados com sucesso!");
-                Console.ResetColor();
+                FeaturedText("Dados adicionados com sucesso!", "Green");
                 PressAnyKey();
                 MainMenu();
             }
@@ -108,26 +102,28 @@ namespace BirthdayApp
                 if (chosenPerson != null)
                 {
                     Console.Clear();
-                    ContrastColor("GREY");
-                    Console.WriteLine("Dados da pessoa:");
-                    Console.ResetColor();
+                    FeaturedText("Dados da pessoa:", "Gray");
                     Console.WriteLine($"ID: {chosenPerson.Id}");
                     Console.WriteLine($"Nome Completo: {chosenPerson.FirstName} {chosenPerson.LastName}");
                     Console.WriteLine($"Data do Aniversário: {chosenPerson.Birthday.ToString("d")}");
+                    if (chosenPerson.CalculateDays() == 0)
+                    {
+                        Console.WriteLine($"Hoje é Aniversário do(a) {chosenPerson.FirstName}! Dê os parabéns!\n");
+                    }
+                    else 
+                    { 
                     Console.WriteLine($"Faltam {chosenPerson.CalculateDays()} dia(s) para esse aniversário.\n");
+                    
+                    }
                 }
                 else
                 {
-                    ContrastColor("Red");
-                    Console.WriteLine("Nenhuma pessoa localizada!");
-                    Console.ResetColor();
+                    FeaturedText("Nenhuma pessoa localizada!", "Red");
                 }
             }
             else
             {
-                ContrastColor("Red");
-                Console.WriteLine("Nenhuma cadastro no sistema. Favor cadastrar uma Pessoa.");
-                Console.ResetColor();
+                FeaturedText("Nenhuma cadastro no sistema. Favor cadastrar uma Pessoa.", "Red");
             }
             PressAnyKey();
             MainMenu();
@@ -136,9 +132,7 @@ namespace BirthdayApp
         static void Update()
         {
             Console.Clear();
-            ContrastColor("Gray");
-            Console.WriteLine("Dados das pessoas:");
-            Console.ResetColor();
+            FeaturedText("Dados da pessoa:", "Gray");
             ShowAllPeople();
             Console.WriteLine("Selecione qual ID você deseja alterar:");
             try
@@ -148,9 +142,7 @@ namespace BirthdayApp
                 Person personSet = new Person();
                 if (personGet == null)
                 {
-                    ContrastColor("Red");
-                    Console.WriteLine("Pessoa não encontrada para o Id forncecido");
-                    Console.ResetColor();
+                    FeaturedText("essoa não encontrada para o Id forncecido", "Red");
                 }
                 else
                 {
@@ -168,27 +160,20 @@ namespace BirthdayApp
                     personSet.Birthday = newBirthday;
 
                     Db.UpdatePerson(personGet, personSet);
-                    ContrastColor("Green");
-                    Console.WriteLine("Dados editados com sucesso!");
-                    Console.ResetColor();
+                    FeaturedText("\nPessoa editada com sucesso!", "Green");
                 }
             }
             catch
             {
-                ContrastColor("Red");
-                Console.WriteLine("Pessoa não encontrada para o Id forncecido");
-                Console.ResetColor();
+                FeaturedText("Pessoa não encontrada para o Id forncecido", "Red");
             }
             PressAnyKey();
             MainMenu();
-
         }
         static void Delete()
         {
             Console.Clear();
-            ContrastColor("Gray");
-            Console.WriteLine("Dados das pessoas:");
-            Console.ResetColor();
+            FeaturedText("Dados das pessoas:", "Gray");
             ShowAllPeople();
             Console.WriteLine("Selecione qual ID você deseja excluir:");
             try
@@ -198,22 +183,16 @@ namespace BirthdayApp
                 if (person != null)
                 {
                     Db.DeletePerson(person);
-                    ContrastColor("Green");
-                    Console.WriteLine("Pessoa excluída com sucesso!");
-                    Console.ResetColor();
+                    FeaturedText("Pessoa excluída com sucesso!", "Green");
                 }
                 else
                 {
-                    ContrastColor("Red");
-                    Console.WriteLine("Pessoa não encontrada para o Id forncecido");
-                    Console.ResetColor();
+                    FeaturedText("Pessoa não encontrada para o Id forncecido", "Red");
                 }
             }
             catch
             {
-                ContrastColor("Red");
-                Console.WriteLine("Pessoa não encontrada para o Id forncecido");
-                Console.ResetColor();
+                FeaturedText("Pessoa não encontrada para o Id forncecido", "Red");
             }
             PressAnyKey();
             MainMenu();
@@ -245,7 +224,6 @@ namespace BirthdayApp
         static void ShowBirthdayToday()
         {
             var today = DateTime.Today;
-            //var today = new DateTime(DateTime.Today.Year, DateTime.TodayMonth, Birthday.Day);
             var select = Db.GetAllPeople().Where(person => person.Birthday.Day.Equals(today.Day) && person.Birthday.Month.Equals(today.Month));
             foreach (var person in select)
             {
@@ -253,19 +231,20 @@ namespace BirthdayApp
             }
         }
 
-        public static void ContrastColor(string color)
+        public static void FeaturedText(string text, string color)
         {
+            Console.ForegroundColor = ConsoleColor.Black;
             switch (color)
             {
                 case "Red": Console.BackgroundColor = ConsoleColor.Red; break;
                 case "Yellow": Console.BackgroundColor = ConsoleColor.Yellow; break;
+                case "Blue": Console.BackgroundColor = ConsoleColor.DarkBlue; break;
                 case "Green": Console.BackgroundColor = ConsoleColor.Green; break;
-                case "Blue": Console.BackgroundColor = ConsoleColor.Blue; break;
                 case "Gray": Console.BackgroundColor = ConsoleColor.Gray; break;
-                default: Console.BackgroundColor = ConsoleColor.Black; break;
-
+                default: Console.ResetColor();break;
             }
-            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(text);
+            Console.ResetColor();
         }
     }
 }
